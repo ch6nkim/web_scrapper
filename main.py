6502a1b378906e83d60aedd314cbbe188d6571fb
Chan <chanhyung.kim1130@gmail.com>
2023-07-02 20:58:56 -0400

    5.12

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 #5.10
-from requests import get
-from bs4 import BeautifulSoup
-from extractors.wwr import extract_wwr_jobs
+# from requests import get
+# from bs4 import BeautifulSoup
+# from extractors.wwr import extract_wwr_jobs
 
-jobs = extract_wwr_jobs("python")
-print(jobs)
+# jobs = extract_wwr_jobs("python")
+# print(jobs)
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+browser = webdriver.Chrome(options=options)
+
+browser.get("https://www.indeed.com/jobs?q=python&limit=50")
+
+# soup = BeautifulSoup(browser.page_source, "html.parser")
+
+# print("test : " ,soup)
+print(browser.page_source)
