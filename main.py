f33255aeaaca2f71c624c0167de7d4d3d06cdc27
Chan <chanhyung.kim1130@gmail.com>
2023-07-02 21:18:24 -0400

    5.13

--- main.py
+++ main.py
@@ -19,7 +19,15 @@ browser = webdriver.Chrome(options=options)
 
 browser.get("https://www.indeed.com/jobs?q=python&limit=50")
 
-# soup = BeautifulSoup(browser.page_source, "html.parser")
+soup = BeautifulSoup(browser.page_source, "html.parser")
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
+jobs = job_list.find_all('li', recursive=False)
+for job in jobs:
+  zone = job.find("div",class_="mosaic-zone")
+  if zone == None:
+    print("job li")
+  else:
+    print("mosaic li") 
 
 # print("test : " ,soup)
-print(browser.page_source)
+# print(browser.page_source)
